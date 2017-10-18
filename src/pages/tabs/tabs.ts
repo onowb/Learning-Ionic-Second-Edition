8ad849d6fdf18fd44c32627ac323098e163c94e4
onowb <onowb007@gmail.com>
2017-10-18 12:13:33 +0700

    Sukses tapi belum komplit

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HomePage} from "../home/home";
+import {AboutPage} from "../about/about";
+import {ContactPage} from "../contact/contact";
 
 /**
  * Generated class for the TabsPage page.
@@ -14,6 +17,11 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
+  // this tells the tabs component which Pages
+  // should be each tab's root Page
+  tab1Root: any = HomePage;
+  tab2Root: any = AboutPage;
+  tab3Root: any = ContactPage;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
